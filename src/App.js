26af47ef44613a03669052f5c0af5755b63b92f5
Giannis Zontanos <johnzodanos@gmail.com>
2022-10-05 13:30:34 +0300

    create project

--- src/App.js
+++ src/App.js
@@ -6,17 +6,12 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <div className='rounded-full bg:black'>
+
+        </div>
+        <p className='text-1xl ring-2 ring-black text-blue-100-800'>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
